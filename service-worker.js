66254389e82f2d922dbf19e8d7eeeb30c0ae604a
poper228 <49150868+poper228@users.noreply.github.com>
2025-07-24 10:36:15 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'flower-app-v1';
+const urlsToCache = [
+  '/flowers.html',
+  '/icons/icon-128.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+});
